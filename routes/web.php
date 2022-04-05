c889c30ef4690421c7519614143a0b20daa98073
Maws <astasene81@gmail.com>
2022-04-05 00:45:07 +0000

    heroku configuration

--- routes/web.php
+++ routes/web.php
@@ -19,29 +19,12 @@ Route::resource('proprietes', ProprieteController::class);
 Route::resource('proprietaires', ProprietaireController::class);
 Route::resource('agences', AgenceController::class);
 
-Route::resource('proprietes', ProprieteController::class);
-Route::resource('proprietaires', ProprietaireController::class);
-Route::resource('agences', AgenceController::class);
-
 Route::get('/', function () {
     return view('welcome');
-    // $user = App\Models\User::first();
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-
 require __DIR__.'/auth.php';
-
-// user protected routes
-// Route::group(['middleware' => ['auth', 'user'], 'prefix' => 'user'], function () {
-//     Route::resource('proprietaires', ProprietaireController::class)->only([
-//         'index', 'update'
-//     ]);
-// });
-// // admin protected routes
-// Route::group(['middleware' => ['auth', 'admin'], 'prefix' => 'admin'], function () {
-//     Route::resource('proprietaires', ProprietaireController::class);
-// });
